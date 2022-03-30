607a2a9678dabff9c750a6f3f977b08ce0a7f62c
alexr <alexr@LAPTOP-UN6S8LEH>
2022-03-30 19:45:22 +0200

    Otra version

--- antequeraRepo/antequeraRepo/Saluda.java
+++ antequeraRepo/antequeraRepo/Saluda.java
@@ -4,7 +4,7 @@ public class Saluda {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola mundo 2");
+		System.out.println("hola mundo 3");
 
 	}
 
